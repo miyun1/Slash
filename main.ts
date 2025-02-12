9a3f9638c784e7bbd642b80262ba4d89de603e84
Ahmad Luqman <44406568+miyun1@users.noreply.github.com>
2025-02-12 23:33:16 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -142,6 +142,17 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         music.play(music.createSoundEffect(WaveShape.Sawtooth, 400, 600, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
     }
 })
+function PlayerHealthSB () {
+    RuyolHPBar = statusbars.create(6, 40, StatusBarKind.Health)
+    RuyolHPBar.setColor(8, 15, 9)
+    RuyolHPBar.setLabel("HP")
+    RuyolHPBar.setBarBorder(2, 15)
+    RuyolHPBar.positionDirection(CollisionDirection.Left)
+    RuyolHPBar.setOffsetPadding(-18, 3)
+    RuyolHPBar.value = 100
+    RuyolHPBar.max = 100
+    RuyolHPBar.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
+}
 function PlayerSetup () {
     stance = 1
     Ruyol = sprites.create(img`
@@ -165,17 +176,9 @@ function PlayerSetup () {
     controller.moveSprite(Ruyol, 80, 0)
     Ruyol.ay = 400
     tiles.placeOnTile(Ruyol, tiles.getTileLocation(2, 6))
-    info.setLife(10)
+    PlayerHealthSB()
     controller.combos.setTimeout(500)
-    RuyolSB = statusbars.create(90, 6, StatusBarKind.Energy)
-    RuyolSB.setColor(5, 15)
-    RuyolSB.setStatusBarFlag(StatusBarFlag.LabelAtEnd, true)
-    RuyolSB.setLabel("Power")
-    RuyolSB.setBarBorder(1, 15)
-    RuyolSB.positionDirection(CollisionDirection.Top)
-    RuyolSB.setOffsetPadding(10, 5)
-    RuyolSB.value = 0
-    RuyolSB.max = 100
+    PlayerPowerSB()
     PlayerAnim()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -184,7 +187,7 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     } else if (stance == 1) {
         stance = 2
     }
-    music.play(music.createSoundEffect(WaveShape.Square, 401, 2076, 148, 209, 100, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
+    music.play(music.createSoundEffect(WaveShape.Square, 1734, 2076, 148, 83, 100, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
     console.log(stance)
 })
 statusbars.onZero(StatusBarKind.catHealth, function (status) {
@@ -962,7 +965,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Peow, function (sprite, otherSprite) {
     music.play(music.createSoundEffect(WaveShape.Sawtooth, 1110, 1066, 255, 0, 150, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
-    info.changeLifeBy(-1)
+    RuyolHPBar.value += -10
     sprite.setFlag(SpriteFlag.GhostThroughSprites, true)
     animation.runImageAnimation(
     sprite,
@@ -1056,13 +1059,29 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Peow, function (sprite, otherSpr
     false
     )
     RuyolSB.value += 5
-    timer.after(800, function () {
+    timer.after(1000, function () {
         sprite.setFlag(SpriteFlag.GhostThroughSprites, false)
     })
 })
 statusbars.onStatusReached(StatusBarKind.catHealth, statusbars.StatusComparison.LTE, statusbars.ComparisonType.Percentage, 50, function (status) {
     music.stopAllSounds()
     music.play(music.createSong(hex`00a0000408040300001c00010a006400f401640000040000000000000000000000000005000004d20000000400010a04000600010d06000800010a08000a00010d0a001000010f10001400010a14001600011116001800010f18002000010d20002400010a24002600010d26002800010a28002a00010d2a003000010f30003800010638004000010840004400010a44004600010d46004800010a48004a00010d4a005000010f50005400010a54005600011156005800010f58006000010d60006400010a64006600010d66006800010a68006a00010f6a007000010d70007400010f74007600011176007800010f78007c0001087c008000010d06001c00010a006400f401640000040000000000000000000000000000000002180000002000010a20004000010d40006000010a60008000010d09010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c800e000000001000100040005000105080009000200060c000d000105100011000100140015000105160017000105180019000200071c001d000105200021000100240025000105280029000200062c002d000105300031000100340035000105360037000105380039000200073c003d000105400041000100440045000105480049000200064c004d000105500051000100540055000105560057000105580059000200075c005d000105600061000100640065000105680069000200066c006d000105700071000100740075000105760077000105780079000200077c007d000105`), music.PlaybackMode.LoopingInBackground)
+})
+function PlayerPowerSB () {
+    RuyolSB = statusbars.create(90, 6, StatusBarKind.Energy)
+    RuyolSB.setColor(5, 15)
+    RuyolSB.setStatusBarFlag(StatusBarFlag.LabelAtEnd, true)
+    RuyolSB.setLabel("Power")
+    RuyolSB.setBarBorder(1, 15)
+    RuyolSB.positionDirection(CollisionDirection.Top)
+    RuyolSB.setOffsetPadding(10, 5)
+    RuyolSB.value = 0
+    RuyolSB.max = 100
+}
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    music.stopAllSounds()
+    music.play(music.createSoundEffect(WaveShape.Sawtooth, 2492, 129, 177, 0, 1000, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
+    game.gameOver(false)
 })
 sprites.onOverlap(SpriteKind.Peow, SpriteKind.Projectile, function (sprite, otherSprite) {
     sprites.destroy(otherSprite, effects.ashes, 50)
@@ -1071,11 +1090,11 @@ sprites.onOverlap(SpriteKind.Peow, SpriteKind.Projectile, function (sprite, othe
         RuyolSB.value += 10
     }
     if (catSB.value <= 50) {
-        catSpeed = randint(90, 110)
+        catSpeed = randint(60, 80)
         catTransform = true
     }
     if (catSB.value <= 25) {
-        catSpeed = randint(90, 110)
+        catSpeed = randint(60, 80)
         catLast = true
     }
 })
@@ -1119,22 +1138,17 @@ sprites.onOverlap(SpriteKind.Peow, SpriteKind.Slash, function (sprite, otherSpri
         RuyolSB.value += 10
     }
     if (catSB.value <= 50) {
-        catSpeed = randint(90, 110)
+        catSpeed = randint(60, 80)
         catTransform = true
     }
     if (catSB.value <= 25) {
-        catSpeed = randint(90, 110)
+        catSpeed = randint(60, 80)
         catLast = true
     }
 })
 function MusicSetup () {
     music.play(music.createSong(assets.song`fightsong`), music.PlaybackMode.LoopingInBackground)
 }
-info.onLifeZero(function () {
-    music.stopAllSounds()
-    music.play(music.createSoundEffect(WaveShape.Sawtooth, 2492, 129, 177, 0, 1000, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
-    game.gameOver(false)
-})
 controller.combos.attachCombo("lrdda", function () {
     if (RuyolSB.value >= 100) {
         ulti()
@@ -1144,7 +1158,7 @@ statusbars.onStatusReached(StatusBarKind.Energy, statusbars.StatusComparison.GTE
     music.play(music.createSoundEffect(WaveShape.Sawtooth, 1333, 2314, 213, 0, 1000, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
 })
 function bossSetup () {
-    catSpeed = 50
+    catSpeed = 40
     CatBoss = sprites.create(img`
         e e e . . . . e e e . . . . 
         c d d c . . c d d c . . . . 
@@ -1164,11 +1178,11 @@ function bossSetup () {
     CatBoss.ay = 400
     CatBoss.setVelocity(50, 0)
     tiles.placeOnTile(CatBoss, tiles.getTileLocation(7, 5))
-    catSB = statusbars.create(6, 40, StatusBarKind.catHealth)
+    catSB = statusbars.create(6, 80, StatusBarKind.catHealth)
     catSB.setColor(2, 15, 4)
     catSB.positionDirection(CollisionDirection.Right)
     catSB.setBarBorder(2, 15)
-    catSB.setOffsetPadding(-20, 6)
+    catSB.setOffsetPadding(0, 6)
     catSB.setLabel("HP")
     catSB.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
     catSB.value = 100
@@ -1863,14 +1877,15 @@ let TPlayer: Sprite = null
 let catLast = false
 let catTransform = false
 let catSB: StatusBarSprite = null
+let RuyolSB: StatusBarSprite = null
 let slashAttack: Sprite = null
 let smallProj: Sprite = null
 let catSpeed = 0
-let RuyolSB: StatusBarSprite = null
 let stance = 0
+let RuyolHPBar: StatusBarSprite = null
 let Ruyol: Sprite = null
 let CatBoss: Sprite = null
-game.splash("SPLASH", "the game")
+game.splash("MOONMAN RIDER", "the game")
 LevelSetup()
 MusicSetup()
 PlayerSetup()
